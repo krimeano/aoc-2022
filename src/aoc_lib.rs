f7cc56b502caeb3f316e2f51463ef066cb8cc74e
spalutin <spalutin@starsgroup.com>
2022-12-03 01:05:26 +0000

    day 1 part 1

--- src/aoc_lib.rs
+++ src/aoc_lib.rs
@@ -1,5 +1,17 @@
+use std::fs;
+
 pub fn make_file_name(is_day: bool, day: u8, variant: Option<u8>) -> String {
     let prefix = if is_day { "day_" } else { "probe_" };
     let suffix = if let Some(x) = variant { format!("_{}", x) } else { String::from("") };
     format!("input/{p}{d}{s}.txt", p = prefix, d = day, s = suffix)
+}
+
+pub fn read_lines(filename: String) -> Vec<String> {
+    // println!("read file: {}", filename);
+    let contents = fs::read_to_string(&filename)
+        .expect(&format!("Something went wrong reading the file {}", &filename));
+
+    contents.split("\n")
+        .map(|x| String::from(x.trim()))
+        .collect()
 }
--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -1,8 +1,27 @@
+use std::cmp::max;
+
+use crate::aoc_lib::read_lines;
+
 pub fn solve_1(file_path: String, verbose: Option<bool>) -> u32 {
     if let Some(true) = verbose {
         println!("SOLVING DAY 1 PART 1 from file {}", file_path)
     }
-    0
+    let lines = read_lines(file_path);
+    let mut current_elf = 0;
+    let mut max_calories = 0;
+    for line in lines {
+        match line.as_str() {
+            "" => {
+                max_calories = max(current_elf, max_calories);
+                current_elf = 0;
+            }
+            _ => match line.parse::<u32>() {
+                Ok(x) => current_elf += x,
+                Err(e) => panic!("{}", e)
+            }
+        }
+    }
+    max_calories
 }
 
 pub fn solve_2(file_path: String, verbose: Option<bool>) -> u32 {
@@ -12,6 +31,7 @@ pub fn solve_2(file_path: String, verbose: Option<bool>) -> u32 {
     0
 }
 
+
 #[cfg(test)]
 mod tests {
     use crate::aoc_lib::make_file_name;
@@ -19,7 +39,7 @@ mod tests {
 
     #[test]
     fn day_1_1() {
-        assert_eq!(solve_1(make_file_name(false, 1, None), None), 24000);
+        assert_eq!(solve_1(make_file_name(false, 1, None), Some(true)), 24000);
     }
 
     #[test]
