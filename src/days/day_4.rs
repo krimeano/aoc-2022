142bb1cabcb9a2b3bd711c68c7dc42b41d80dfdb
spalutin <spalutin@starsgroup.com>
2022-12-04 11:27:15 +0000

    day 4 2

--- src/days/day_4.rs
+++ src/days/day_4.rs
@@ -1,21 +1,43 @@
 pub fn solve_1(input_lines: &[String], _verbose: Option<bool>) -> u32 {
+    solve(input_lines, check_contain)
+}
+
+pub fn solve_2(input_lines: &[String], _verbose: Option<bool>) -> u32 {
+    solve(input_lines, check_overlap)
+}
+
+fn solve(input_lines: &[String], check: fn(&Vec<Vec<u8>>) -> bool) -> u32 {
     input_lines
         .iter()
         .filter(|x| !x.is_empty())
         .map(|x| {
             x.split(',')
                 .map(|y| y.split('-').map(|z| z.parse().unwrap()).collect())
-                .collect::<Vec<Vec<u32>>>()
+                .collect::<Vec<Vec<u8>>>()
         })
-        .filter(|x| {
-            (x[0][0] <= x[1][0] && x[0][1] >= x[1][1]) || (x[0][0] >= x[1][0] && x[0][1] <= x[1][1])
-        })
+        .filter(|x| check(x))
         .count() as u32
 }
 
-pub fn solve_2(_input_lines: &[String], _verbose: Option<bool>) -> u32 {
-    0
+fn check_contain(pair: &Vec<Vec<u8>>) -> bool {
+    let a = pair[0][0];
+    let b = pair[0][1];
+    let c = pair[1][0];
+    let d = pair[1][1];
+    (a <= c && b >= d) || (a >= c && b <= d)
 }
+
+fn check_overlap(pair: &Vec<Vec<u8>>) -> bool {
+    let a = pair[0][0];
+    let b = pair[0][1];
+    let c = pair[1][0];
+    let d = pair[1][1];
+    (a >= c && a <= d)
+        || (b >= c && b <= d)
+        || (c >= a && c <= b)
+        || (d >= a && d <= b)
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -31,6 +53,6 @@ mod tests {
     #[test]
     fn part_2() {
         let probe = read_probe(4, None);
-        assert_eq!(solve_2(&probe, Some(true)), 0);
+        assert_eq!(solve_2(&probe, Some(true)), 4);
     }
 }
