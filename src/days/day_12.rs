e9593989364aa52248219ed30af254b98ce8748c
spalutin <spalutin@starsgroup.com>
2022-12-17 22:57:17 +0000

    day 12 1

--- src/days/day_12.rs
+++ src/days/day_12.rs
@@ -1,140 +1,171 @@
-pub fn solve_1(input_lines: &[String], verbose: bool) -> i32 {
-    let heights = get_heights(input_lines);
-    let h = heights.len();
+#[derive(Debug)]
+struct Cell {
+    c: char,
+    // height
+    h: u8,
+    // steps to reach
+    s: Option<usize>,
+    // came from
+    f: Option<[usize; 2]>,
+    // going to
+    t: Option<[usize; 2]>,
+}
+
+impl Cell {
+    pub fn new(c: char) -> Self {
+        let h = match c {
+            'S' => 1,
+            'E' => 26,
+            _ => c as u8 - 'a' as u8 + 1,
+        };
+        Self {
+            c,
+            h,
+            s: None,
+            f: None,
+            t: None,
+        }
+    }
+}
+
+pub fn solve_1(input_lines: &[String], verbose: bool) -> usize {
+    let [[ix0, jy0], [ix1, jy1]] = find_points(&input_lines);
+    if verbose {
+        println!("{:#?}", input_lines);
+        println!("start  at {:?}", (ix0, jy0));
+        println!("finish at {:?}", (ix1, jy1));
+    }
+    let mut cells: Vec<Vec<Cell>> = input_lines
+        .iter()
+        .filter(|line| !line.is_empty())
+        .map(|line| line.chars().map(|c| Cell::new(c)).collect())
+        .collect();
+    cells[ix0][jy0].s = Some(0);
+    let h = cells.len();
     if h == 0 {
         return 0;
     }
-    let w = heights[0].len();
+    let w = cells[0].len();
     if w == 0 {
         return 0;
     }
-    let hw = (h * w) as i32;
-
-    let mut steps: Vec<Vec<i32>> = (0..h).map(|_| (0..w).map(|_| 999).collect()).collect();
-
-    let ((ix0, jy0), (ix1, jy1)) = find_points(&heights);
-
-    steps[ix0][jy0] = 0;
-    let mut current_cells = Vec::from([(ix0, jy0)]);
-    if verbose {
-        println!("start  at {:?}", (ix0, jy0));
-        println!("finish at {:?}", (ix1, jy1));
-        print_map(&heights);
-        println!();
-        print_map(&steps);
-        println!();
-    }
-
-    for current_steps in 1..=hw {
+    let mut current_cells = Vec::from([[ix0, jy0]]);
+    let mut came = false;
+    for current_steps in 1..=h * w {
+        if came || current_cells.is_empty() {
+            break;
+        }
         let mut new_cells = Vec::new();
         if verbose {
             println!("{:?}", current_cells);
         }
-        for (ix_c, jy_c) in current_cells {
+        for [ix_c, jy_c] in current_cells {
+            if came {
+                break;
+            }
             if verbose {
-                println!("from {:?}, {} steps", (ix_c, jy_c), current_steps - 1);
+                println!("from {:?}, {:?}", [ix_c, jy_c], cells[ix_c][jy_c]);
             }
 
-            for (ix, jy) in get_neighbours(ix_c, jy_c, h, w) {
-                if steps[ix][jy] <= current_steps {
+            for [ix, jy] in get_neighbours([ix_c, jy_c], [h, w]) {
+                if came {
+                    break;
+                }
+                if cells[ix][jy].s != None || cells[ix][jy].h > cells[ix_c][jy_c].h + 1 {
                     continue;
                 }
-                if heights[ix][jy] - heights[ix_c][jy_c] > 1 {
-                    continue;
+                cells[ix][jy].s = Some(current_steps);
+                cells[ix][jy].f = Some([ix_c, jy_c]);
+                if verbose {
+                    println!("\tneighbour {:?}, {:?}", [ix, jy], cells[ix][jy]);
                 }
-
-                steps[ix][jy] = current_steps;
-
                 if ix == ix1 && jy == jy1 {
                     if verbose {
                         println!("ARRIVED! {} steps", current_steps);
-                        print_map(&heights);
-                        println!();
-                        print_map(&steps);
-                        println!();
-                        println!("ARRIVED! {} steps", current_steps);
                     }
-                    return current_steps;
+                    came = true;
                 }
-                if verbose {
-                    println!("\tneighbour {:?}, {} steps", (ix, jy), current_steps);
-                }
-
-                new_cells.push((ix, jy));
+                new_cells.push([ix, jy]);
             }
         }
-        if verbose {
-            print_map(&heights);
-            println!();
-            print_map(&steps);
-            println!();
-        }
-
         current_cells = new_cells;
     }
-    0
+
+    if verbose {
+        let mut ix_c = ix1;
+        let mut jy_c = jy1;
+        while cells[ix_c][jy_c].f != None {
+            let [ix, jy] = cells[ix_c][jy_c].f.unwrap();
+            cells[ix][jy].t = Some([ix_c, jy_c]);
+            ix_c = ix;
+            jy_c = jy;
+        }
+        for ix in 0..h {
+            println!("{}", input_lines[ix]);
+        }
+        println!();
+        for ix in 0..h {
+            for jy in 0..w {
+                let c = if cells[ix][jy].c == 'E' {
+                    'E'
+                } else {
+                    match cells[ix][jy].t {
+                        None => '.',
+                        Some([to_ix, to_jy]) => {
+                            if to_ix < ix {
+                                '^'
+                            } else if to_ix > ix {
+                                'v'
+                            } else if to_jy < jy {
+                                '<'
+                            } else if to_jy > jy {
+                                '>'
+                            } else {
+                                'X'
+                            }
+                        }
+                    }
+                };
+                print!("{}", c);
+            }
+            println!();
+        }
+    }
+    cells[ix1][jy1].s.unwrap()
 }
 
 pub fn solve_2(_input_lines: &[String], _verbose: bool) -> i32 {
     0
 }
 
-fn get_heights(input_lines: &[String]) -> Vec<Vec<i32>> {
-    let mut out = Vec::new();
-    for line in input_lines {
-        if line.is_empty() {
-            continue;
-        }
-        let row = line
-            .chars()
-            .map(|x| match x {
-                'S' => 0,
-                'E' => 27,
-                _ => x as i32 - 'a' as i32 + 1,
-            })
-            .collect();
-        out.push(row);
-    }
-    out
-}
-
-fn find_points(heights: &[Vec<i32>]) -> ((usize, usize), (usize, usize)) {
-    let mut start = (0, 0);
-    let mut finish = (0, 0);
-    for ix in 0..heights.len() {
-        for jy in 0..heights[ix].len() {
-            if heights[ix][jy] == 0 {
-                start = (ix, jy)
-            } else if heights[ix][jy] == 27 {
-                finish = (ix, jy)
+fn find_points(lines: &[String]) -> [[usize; 2]; 2] {
+    let mut start: Option<[usize; 2]> = None;
+    let mut finish: Option<[usize; 2]> = None;
+    for (ix, line) in lines.iter().enumerate() {
+        for (jy, c) in line.chars().enumerate() {
+            if c == 'S' {
+                start = Some([ix, jy])
+            } else if c == 'E' {
+                finish = Some([ix, jy])
             }
         }
     }
-    (start, finish)
+    [start.unwrap(), finish.unwrap()]
 }
 
-fn print_map(xxx: &[Vec<i32>]) {
-    for xx in xxx {
-        for x in xx {
-            print!("{:4?}", x);
-        }
-        println!();
-    }
-}
-
-fn get_neighbours(ix: usize, jy: usize, h: usize, w: usize) -> Vec<(usize, usize)> {
-    let mut out = Vec::new();
+fn get_neighbours([ix, jy]: [usize; 2], [h, w]: [usize; 2]) -> Vec<[usize; 2]> {
+    let mut out = vec![];
     if ix > 0 {
-        out.push((ix - 1, jy));
+        out.push([ix - 1, jy]);
     }
     if jy > 0 {
-        out.push((ix, jy - 1));
+        out.push([ix, jy - 1]);
     }
     if ix + 1 < h {
-        out.push((ix + 1, jy));
+        out.push([ix + 1, jy]);
     }
     if jy + 1 < w {
-        out.push((ix, jy + 1));
+        out.push([ix, jy + 1]);
     }
     out
 }
