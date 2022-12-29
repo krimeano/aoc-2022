ffa67c285afbd27d89928eee3cbdae4559e2422a
spalutin <spalutin@starsgroup.com>
2022-12-29 20:37:52 +0000

    day 21 2

--- src/days/day_21.rs
+++ src/days/day_21.rs
@@ -9,6 +9,7 @@ struct Monkey {
 
 #[derive(Debug)]
 struct MonkeyIndex {
+    part_two: bool,
     verbose: bool,
     indexes: HashMap<String, usize>,
     monkeys: HashMap<usize, Monkey>,
@@ -18,8 +19,9 @@ struct MonkeyIndex {
 }
 
 impl MonkeyIndex {
-    pub fn new(verbose: bool) -> Self {
+    pub fn new(part_two: bool, verbose: bool) -> Self {
         Self {
+            part_two,
             verbose,
             indexes: HashMap::new(),
             monkeys: HashMap::new(),
@@ -31,30 +33,118 @@ impl MonkeyIndex {
 
     pub fn yell(&mut self) -> i64 {
         let order = self.arrange();
+        let mut stack_part_two = Vec::new();
         if self.verbose {
             println!("order = {:?}", order);
         }
         for ix in order {
-            let mut monkey = self.monkeys.get(&ix).unwrap();
+            let monkey = self.monkeys.get(&ix).unwrap();
+
             if let Some(dd) = self.depends_on.get(&ix) {
-                let vv = dd
-                    .iter()
-                    .map(|d| *self.values.get(d).unwrap())
-                    .collect::<Vec<i64>>();
-                let value = match monkey.operation {
-                    Some('-') => vv[0] - vv[1],
-                    Some('+') => vv[0] + vv[1],
-                    Some('*') => vv[0] * vv[1],
-                    Some('/') => vv[0] / vv[1],
-                    _ => panic!(),
-                };
-                self.values.insert(ix, value);
-            }
-            if self.verbose {
-                println!("{} yells {}", monkey.name, self.values.get(&ix).unwrap());
+                let v_a = self.values.get(&dd[0]);
+                let v_b = self.values.get(&dd[1]);
+
+                if v_a.is_some() && v_b.is_some() {
+                    let a = *v_a.unwrap();
+                    let b = *v_b.unwrap();
+                    let value = match monkey.operation {
+                        Some('+') => a + b,
+                        Some('*') => a * b,
+                        Some('-') => a - b,
+                        Some('/') => a / b,
+                        _ => panic!(),
+                    };
+                    self.values.insert(ix, value);
+                    if self.verbose {
+                        println!("{} yells {}", monkey.name, value);
+                    }
+                } else {
+                    stack_part_two.push(ix);
+                    if self.verbose {
+                        println!("omit monkey {}, yell later", monkey.name);
+                    }
+                }
+            } else {
+                let value = self.values.get(&ix);
+                if value.is_none() {
+                    stack_part_two.push(ix);
+                    if self.verbose {
+                        println!("omit monkey {}, yell later", monkey.name);
+                    }
+                } else {
+                    if self.verbose {
+                        println!("{} yells {:?}", monkey.name, value.unwrap());
+                    }
+                }
             }
         }
-        *self.values.get(self.indexes.get("root").unwrap()).unwrap()
+
+        if self.verbose {
+            println!("stack part two = {:?}", stack_part_two);
+        }
+
+        while let Some(ix) = stack_part_two.pop() {
+            let monkey = self.monkeys.get(&ix).unwrap();
+            let current_value = self.values.get(&ix);
+
+            if self.verbose {
+                println!("reversing monkey {:?}", monkey);
+            }
+            if let Some([left, right]) = self.depends_on.get(&ix) {
+                let left_value = self.values.get(left);
+                let right_value = self.values.get(right);
+                let jy = if left_value.is_none() { left } else { right };
+                let value = match monkey.operation {
+                    Some('=') => {
+                        if left_value.is_none() {
+                            *right_value.unwrap()
+                        } else {
+                            *left_value.unwrap()
+                        }
+                    }
+                    Some('+') => {
+                        if left_value.is_none() {
+                            println!("{:?}", monkey);
+                            current_value.unwrap() - right_value.unwrap()
+                        } else {
+                            current_value.unwrap() - left_value.unwrap()
+                        }
+                    }
+                    Some('*') => {
+                        if left_value.is_none() {
+                            current_value.unwrap() / right_value.unwrap()
+                        } else {
+                            current_value.unwrap() / left_value.unwrap()
+                        }
+                    }
+                    Some('-') => {
+                        if left_value.is_none() {
+                            current_value.unwrap() + right_value.unwrap()
+                        } else {
+                            left_value.unwrap() - current_value.unwrap()
+                        }
+                    }
+                    Some('/') => {
+                        if left_value.is_none() {
+                            current_value.unwrap() * right_value.unwrap()
+                        } else {
+                            left_value.unwrap() / current_value.unwrap()
+                        }
+                    }
+                    _ => panic!(),
+                };
+                self.values.insert(*jy, value);
+                if self.verbose {
+                    println!("monkey {} should have yell {}", jy, value);
+                }
+            }
+        }
+
+        let last_monkey_name = if self.part_two { "humn" } else { "root" };
+        *self
+            .values
+            .get(self.indexes.get(last_monkey_name).unwrap())
+            .unwrap()
     }
 
     pub fn add_monkey(&mut self, line: &str) {
@@ -78,7 +168,9 @@ impl MonkeyIndex {
 
         match parts.len() {
             1 => {
-                self.values.insert(ix, parts[0].parse::<i64>().unwrap());
+                if !self.part_two || monkey_data[0] != "humn".to_string() {
+                    self.values.insert(ix, parts[0].parse::<i64>().unwrap());
+                }
             }
             3 => {
                 let jy = self.get_ix(&parts[0]);
@@ -86,7 +178,12 @@ impl MonkeyIndex {
                 self.depends_on.insert(ix, [jy, kz]);
                 self.depending.insert(jy, ix);
                 self.depending.insert(kz, ix);
-                monkey.operation = parts[1].chars().next();
+
+                monkey.operation = if monkey_data[0] == "root".to_string() && self.part_two {
+                    Some('=')
+                } else {
+                    parts[1].chars().next()
+                };
             }
             _ => panic!("wrong input! {:?}", line),
         }
@@ -156,7 +253,7 @@ impl MonkeyIndex {
 }
 
 pub fn solve_1(input_lines: &[String], verbose: bool) -> i64 {
-    let mut monkeys = MonkeyIndex::new(verbose);
+    let mut monkeys = MonkeyIndex::new(false, verbose);
     for line in input_lines {
         if !line.is_empty() {
             monkeys.add_monkey(line)
@@ -165,8 +262,14 @@ pub fn solve_1(input_lines: &[String], verbose: bool) -> i64 {
     monkeys.yell()
 }
 
-pub fn solve_2(_input_lines: &[String], _verbose: bool) -> i64 {
-    0
+pub fn solve_2(input_lines: &[String], verbose: bool) -> i64 {
+    let mut monkeys = MonkeyIndex::new(true, verbose);
+    for line in input_lines {
+        if !line.is_empty() {
+            monkeys.add_monkey(line)
+        }
+    }
+    monkeys.yell()
 }
 
 #[cfg(test)]
@@ -183,6 +286,6 @@ mod tests {
     #[test]
     fn part_2() {
         let probe = read_probe(21, None);
-        assert_eq!(solve_2(&probe, true), 0);
+        assert_eq!(solve_2(&probe, false), 301);
     }
 }
